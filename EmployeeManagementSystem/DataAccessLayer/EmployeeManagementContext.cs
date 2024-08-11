fce407e2e342d01802ada7678199631873b6d375
PahmNogcHa2003 <pnha03dev.it@gmail.com>
2024-08-12 00:36:25 +0700

    Create folder BO, DAL, Repositories, Services, View and using EntityFramework

---
+++ EmployeeManagementSystem/DataAccessLayer/EmployeeManagementContext.cs
@@ -0,0 +1,202 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace BusinessObjects;
+
+public partial class EmployeeManagementContext : DbContext
+{
+    public EmployeeManagementContext()
+    {
+    }
+
+    public EmployeeManagementContext(DbContextOptions<EmployeeManagementContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<AccountMember> AccountMembers { get; set; }
+
+    public virtual DbSet<Country> Countries { get; set; }
+
+    public virtual DbSet<Department> Departments { get; set; }
+
+    public virtual DbSet<Employee> Employees { get; set; }
+
+    public virtual DbSet<Job> Jobs { get; set; }
+
+    public virtual DbSet<Location> Locations { get; set; }
+
+    public virtual DbSet<Region> Regions { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        var builder = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+        IConfiguration configuration = builder.Build();
+        optionsBuilder.UseSqlServer(configuration.GetConnectionString("DefaultConnection"));
+    }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<AccountMember>(entity =>
+        {
+            entity.HasKey(e => e.MemberId).HasName("PK__AccountM__0CF04B3854AD8928");
+
+            entity.ToTable("AccountMember");
+
+            entity.Property(e => e.MemberId)
+                .HasMaxLength(50)
+                .IsUnicode(false)
+                .HasColumnName("MemberID");
+            entity.Property(e => e.Password)
+                .HasMaxLength(255)
+                .IsUnicode(false);
+        });
+
+        modelBuilder.Entity<Country>(entity =>
+        {
+            entity.HasKey(e => e.CountryId).HasName("PK__Countrie__10D160BFCDB504A1");
+
+            entity.Property(e => e.CountryId)
+                .HasMaxLength(10)
+                .IsUnicode(false)
+                .HasColumnName("CountryID");
+            entity.Property(e => e.CountryName)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+            entity.Property(e => e.RegionId).HasColumnName("RegionID");
+
+            entity.HasOne(d => d.Region).WithMany(p => p.Countries)
+                .HasForeignKey(d => d.RegionId)
+                .HasConstraintName("FK__Countries__Regio__300424B4");
+        });
+
+        modelBuilder.Entity<Department>(entity =>
+        {
+            entity.HasKey(e => e.DepartmentId).HasName("PK__Departme__B2079BCD7ECBE74E");
+
+            entity.Property(e => e.DepartmentId)
+                .ValueGeneratedNever()
+                .HasColumnName("DepartmentID");
+            entity.Property(e => e.DepartmentName)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+            entity.Property(e => e.LocationId)
+                .HasMaxLength(5)
+                .IsUnicode(false)
+                .HasColumnName("LocationID");
+            entity.Property(e => e.ManagerId).HasColumnName("ManagerID");
+
+            entity.HasOne(d => d.Location).WithMany(p => p.Departments)
+                .HasForeignKey(d => d.LocationId)
+                .HasConstraintName("FK__Departmen__Locat__30F848ED");
+
+            entity.HasOne(d => d.Manager).WithMany(p => p.Departments)
+                .HasForeignKey(d => d.ManagerId)
+                .HasConstraintName("FK__Departmen__Manag__31EC6D26");
+        });
+
+        modelBuilder.Entity<Employee>(entity =>
+        {
+            entity.HasKey(e => e.EmployeeId).HasName("PK__Employee__7AD04FF1CEEEEDED");
+
+            entity.Property(e => e.EmployeeId)
+                .ValueGeneratedNever()
+                .HasColumnName("EmployeeID");
+            entity.Property(e => e.CommissionPct).HasColumnName("Commission_pct");
+            entity.Property(e => e.DepartmentId).HasColumnName("DepartmentID");
+            entity.Property(e => e.Email)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+            entity.Property(e => e.FirstName)
+                .HasMaxLength(30)
+                .IsUnicode(false);
+            entity.Property(e => e.JobId)
+                .HasMaxLength(20)
+                .IsUnicode(false)
+                .HasColumnName("JobID");
+            entity.Property(e => e.LastName)
+                .HasMaxLength(30)
+                .IsUnicode(false);
+            entity.Property(e => e.ManagerId).HasColumnName("ManagerID");
+            entity.Property(e => e.Phone)
+                .HasMaxLength(20)
+                .IsUnicode(false);
+
+            entity.HasOne(d => d.Department).WithMany(p => p.Employees)
+                .HasForeignKey(d => d.DepartmentId)
+                .HasConstraintName("FK__Employees__Depar__32E0915F");
+
+            entity.HasOne(d => d.Job).WithMany(p => p.Employees)
+                .HasForeignKey(d => d.JobId)
+                .HasConstraintName("FK__Employees__JobID__33D4B598");
+
+            entity.HasOne(d => d.Manager).WithMany(p => p.InverseManager)
+                .HasForeignKey(d => d.ManagerId)
+                .HasConstraintName("FK__Employees__Manag__34C8D9D1");
+        });
+
+        modelBuilder.Entity<Job>(entity =>
+        {
+            entity.HasKey(e => e.JobId).HasName("PK__Jobs__056690E255DFA6D6");
+
+            entity.Property(e => e.JobId)
+                .HasMaxLength(20)
+                .IsUnicode(false)
+                .HasColumnName("JobID");
+            entity.Property(e => e.JobTitle)
+                .HasMaxLength(100)
+                .IsUnicode(false);
+            entity.Property(e => e.MaxSalary).HasColumnName("max_salary");
+            entity.Property(e => e.MinSalary).HasColumnName("min_salary");
+        });
+
+        modelBuilder.Entity<Location>(entity =>
+        {
+            entity.HasKey(e => e.LocationId).HasName("PK__Location__E7FEA477094E0EA2");
+
+            entity.Property(e => e.LocationId)
+                .HasMaxLength(5)
+                .IsUnicode(false)
+                .HasColumnName("LocationID");
+            entity.Property(e => e.City)
+                .HasMaxLength(30)
+                .IsUnicode(false);
+            entity.Property(e => e.CountryId)
+                .HasMaxLength(10)
+                .IsUnicode(false)
+                .HasColumnName("CountryID");
+            entity.Property(e => e.PostalCode)
+                .HasMaxLength(20)
+                .IsUnicode(false);
+            entity.Property(e => e.StateProvince)
+                .HasMaxLength(30)
+                .IsUnicode(false);
+            entity.Property(e => e.StreetAddress)
+                .HasMaxLength(100)
+                .IsUnicode(false);
+
+            entity.HasOne(d => d.Country).WithMany(p => p.Locations)
+                .HasForeignKey(d => d.CountryId)
+                .HasConstraintName("FK__Locations__Count__35BCFE0A");
+        });
+
+        modelBuilder.Entity<Region>(entity =>
+        {
+            entity.HasKey(e => e.RegionId).HasName("PK__Regions__ACD844434E2A28EF");
+
+            entity.Property(e => e.RegionId)
+                .ValueGeneratedNever()
+                .HasColumnName("RegionID");
+            entity.Property(e => e.RegionName)
+                .HasMaxLength(30)
+                .IsUnicode(false);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
